b78dbb01dc12531d1e5c26a1030a3ce9cbc621d6
xamarinskills <sumitsisodia.xamarinskills@gmail.com>
2018-05-07 21:38:50 +0530

    Add project files.

---
+++ XFPopupAnimation/XFPopupAnimation/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFPopupAnimation
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushPopupAsync(new LoginPopupPage());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushPopupAsync(new RegPage());
+        }
+    }
+}
